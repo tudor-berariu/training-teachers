69b8f8bbe50c6a8476cf92c44ce04b624f2c37c4
Stefan Postavaru <spostavaru@gmail.com>
2019-01-11 15:51:52 +0200

    variable memory

---
+++ tf_utils.py
@@ -0,0 +1,139 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.contrib.layers import xavier_initializer
+from tensorflow.keras.datasets import fashion_mnist
+from tensorflow.distributions import Categorical
+import scipy as sp
+
+
+
+def covariance(x):
+  mean = tf.reduce_mean(x,axis=0)
+  x = x-mean
+  var = tf.reduce_mean(tf.square(x),axis=0)
+  return tf.transpose(x)@x/var
+
+
+def save_img(data1, data2, path, n=100):
+  if len(data1.shape)==2:
+    l = int(np.sqrt(int(data1.shape[1])))
+    data1 = np.reshape(data1, (-1,l,l,1))
+    data2 = np.reshape(data2, (-1,l,l,1))
+  else:
+    data1=np.squeeze(data1,-1)
+    data2=np.squeeze(data2,-1)
+    l=int(data1.shape[1])
+  data = np.concatenate((data1,data2),axis=-1)
+  data = np.reshape(data[:n], [-1,2*l])
+  sp.misc.toimage(data, cmin=0,cmax=1).save(path)
+
+def kl(f, g, axis=-1):
+  f_ = tf.nn.softmax(f)
+  g_ = tf.nn.softmax(g)
+  # return tf.reduce_sum(f_*(tf.log(f_)-tf.log(g_)),axis=axis)
+  return tf.distributions.kl_divergence(Categorical(f_),Categorical(g_))
+
+def simple_img(data, path, n=20):
+  n=min(n,len(data))
+  l=int(data.shape[1])
+  data=np.squeeze(data,-1)
+  data=np.reshape(data[:n],[-1,l])
+  sp.misc.toimage(data, cmin=0,cmax=1).save(path)
+
+
+class LinProj(object):
+
+  def __init__(self, name, args):
+    self.name=name
+    self.sizes = [*args.in_size]+args.lin.layers+[args.out_size]
+    self.w = []
+    self.b = []
+    self.args = args
+    with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
+      for i in range(len(self.sizes)-1):
+        self.w.append(tf.get_variable(f'w_{i}',[self.sizes[i],self.sizes[i+1]],
+          initializer=xavier_initializer()))
+        self.b.append(tf.get_variable(f'b_{i}', [1,self.sizes[i+1]],
+          initializer=tf.initializers.constant(0)))
+
+  def __call__(self,x):
+    with tf.variable_scope(self.name, reuse=tf.AUTO_REUSE):
+      for i,(w,b) in enumerate(zip(self.w,self.b)):
+        x = tf.matmul(x,w)+b
+        if i != len(self.w)-1:
+          if self.args.activation == 'tanh':
+            x = tf.tanh(x)
+          elif self.args.activation == 'relu':
+            x = tf.nn.relu(x) 
+    return x
+
+  @property
+  def vars(self):
+    ret = list(self.w)
+    ret.extend(self.b)
+    return ret
+
+
+class ConvProj(object):
+
+  def __init__(self,name, args):
+    self.name=name
+    self.w = []
+    self.b = []
+    self.args = args
+    in_size=1
+    self.layers = []
+    self.layers.extend(args.conv.layers)
+    self.layers.append([args.out_size])
+    self.dropout = args.conv.dropout
+    with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
+      first = True
+      for i,layer in enumerate(self.layers):
+        if len(layer)==3:
+          self.w.append(tf.get_variable(f'w_{i}',
+                        [layer[0],layer[1],in_size,layer[2]],
+                          initializer=xavier_initializer()))
+        else:
+          if len(layer)!=3 and first:
+            first=False
+            in_size = 7*7*16
+          self.w.append(tf.get_variable(f'w_{i}',[in_size,layer[-1]],
+                        initializer=xavier_initializer()))
+        self.b.append(tf.get_variable(f'b_{i}', [1,layer[-1]],
+                      initializer=tf.initializers.constant(0)))
+        in_size = layer[-1]
+
+  def __call__(self,x, training):
+    first=True
+    with tf.variable_scope(self.name, reuse=tf.AUTO_REUSE):
+      for i,(w,b) in enumerate(zip(self.w,self.b)):
+        if len(w.shape)==4:
+          x = tf.nn.conv2d(x,w,[1,1,1,1],'SAME')+b
+          x = tf.nn.max_pool(x,[1,2,2,1],[1,2,2,1],'SAME')
+        else:
+          if first:
+            first = False
+            x = tf.layers.flatten(x)
+          x = tf.matmul(x,w)+b
+        if i != len(self.w)-1:
+          x = tf.layers.dropout(x, self.dropout, training=training)
+          if self.args.activation == 'tanh':
+            x = tf.tanh(x)
+          elif self.args.activation == 'relu':
+            x = tf.nn.relu(x)
+    return x 
+
+  @property
+  def vars(self):
+    ret = list(self.w)
+    ret.extend(self.b)
+    return ret
+
+def cosine(a,b,axis=-1):
+  return tf.reduce_sum(a*b,axis=axis)/\
+          (tf.norm(a,axis=axis)*tf.norm(b,axis=axis))
+      
+
+def acc(labels,pred):
+  return tf.reduce_mean(tf.cast(tf.equal(tf.argmax(labels,axis=-1),
+                                  tf.argmax(pred,axis=-1)),tf.float32))    
---
+++ variable_memory.py
@@ -0,0 +1,231 @@
+from liftoff.config import read_config
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.datasets import fashion_mnist
+from numpy.random import permutation
+from tf_utils import ConvProj, kl, cosine, save_img, covariance
+import tf_utils
+import os
+
+def get_dataset(args):
+    if args.dataset == 'fashion_mnist':
+        (train_data, train_labels), (test_data,test_labels) = \
+            fashion_mnist.load_data()
+        train_data = train_data.astype(np.float32)/255
+        test_data = test_data.astype(np.float32)/255
+        return (train_data[:,:,:,np.newaxis], train_labels), \
+                (test_data[:,:,:,np.newaxis],test_labels)
+
+
+def create_session():
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    return tf.Session(config=config)
+
+
+def create_students(args):
+    students = []
+    students_params = []
+    for i in range(args.n_proj):
+        students.append(ConvProj(f'student_{i}',args))
+        students_params.extend(students[-1].vars)
+    return students, students_params
+
+
+
+
+
+def run(args):
+    if args.out_dir is None:
+        args.out_dir = './junk/'
+    s = create_session()
+    logs_writer = tf.summary.FileWriter(args.out_dir, s.graph)
+    (train_data, train_labels), (test_data, test_labels) = get_dataset(args)
+    fake_data = tf.get_variable('fake_data', 
+                            [args.memory_size, *train_data.shape[1:]],
+                            initializer = tf.initializers.random_normal())
+    fake_labels = tf.get_variable('fake_labels',
+                            [args.memory_size, 10],
+                            initializer = tf.initializers.random_normal())
+    students, students_params = create_students(args)
+
+    full_real_grad = []
+    full_fake_grad = []
+    training = tf.placeholder_with_default(True,None)
+    batch_data_ = tf.placeholder(tf.float32, [None,28,28,1],
+                                'data_placeholder')
+    batch_test_data_ = tf.placeholder(tf.float32,[None,28,28,1])
+    batch_labels_ = tf.placeholder(tf.int32,[None],'labels_placeholder')
+    batch_labels_onehot = tf.one_hot(batch_labels_, 10, dtype=tf.float32)
+    batch_test_labels_ = tf.placeholder(tf.int32,[None])
+    batch_test_labels_onehot = tf.one_hot(batch_test_labels_,
+                                        10,dtype=tf.float32)
+    memory_indices_ = tf.placeholder(tf.int32,[None],'indices_placeholder')
+    batch_fake_data = tf.gather(fake_data, memory_indices_)
+    batch_fake_labels = tf.nn.softmax(tf.gather(\
+                            fake_labels, memory_indices_))
+    full_real_grad, full_fake_grad = [], []
+    real_acc, fake_acc, test_acc = 0, 0, 0
+    total_real_loss, total_fake_loss, total_test_loss = 0, 0, 0
+    for student in students:
+        real_output = student(batch_data_,training)
+        fake_output = student(batch_fake_data, training)
+        test_output = student(batch_test_data_, False)
+
+        real_loss = kl(batch_labels_onehot,tf.nn.softmax(real_output))
+        fake_loss = kl(batch_fake_labels, tf.nn.softmax(fake_output))
+        test_loss = kl(batch_test_labels_onehot, 
+                        tf.nn.softmax(test_output))
+        real_loss = tf.reduce_mean(real_loss)
+        fake_loss = tf.reduce_mean(fake_loss)
+        test_loss = tf.reduce_mean(test_loss)
+
+        total_real_loss += real_loss/args.n_proj
+        total_fake_loss += fake_loss/args.n_proj
+        total_test_loss += test_loss/args.n_proj
+
+        real_acc+=tf_utils.acc(real_output, batch_labels_onehot)/args.n_proj
+        fake_acc+=tf_utils.acc(fake_output, batch_fake_labels)/args.n_proj
+
+        real_grad = tf.gradients(real_loss, student.vars)
+        real_grad = [tf.reshape(v,[-1]) for v in real_grad]
+        full_real_grad.extend(real_grad)
+        fake_grad = tf.gradients(fake_loss, student.vars)
+        fake_grad = [tf.reshape(v,[-1]) for v in fake_grad]
+        full_fake_grad.extend(fake_grad)
+
+    full_real_grad = tf.concat(full_real_grad,0)
+    full_fake_grad = tf.concat(full_fake_grad,0)
+
+    objective = -cosine(full_real_grad, full_fake_grad)/args.n_proj
+    prof_optimizer = tf.train.AdamOptimizer(learning_rate=args.lr)
+    prof_optim_step = prof_optimizer.minimize(objective, 
+                                    var_list=[fake_data,fake_labels])
+    train_optimizer = tf.train.AdamOptimizer(learning_rate=1e-3)
+    train_optim_step = train_optimizer.minimize(total_real_loss,
+                                    var_list=students_params)
+    test_optimizer = tf.train.AdamOptimizer(learning_rate=1e-3)
+    test_optim_step = test_optimizer.minimize(total_fake_loss, 
+                                            var_list=students_params)
+    reset_students = tf.initializers.variables(students_params)
+    reset_train_optim = tf.initializers.variables(train_optimizer.variables())
+    s.run(tf.global_variables_initializer())
+    s.run(tf.local_variables_initializer())
+    tf.summary.scalar('grad_cos', objective)
+    tf.summary.scalar('train_real_acc', real_acc)
+    tf.summary.scalar('train_fake_acc', fake_acc)
+    tf.summary.scalar('train_real_loss', total_real_loss)
+    tf.summary.scalar('train_fake_loss', total_fake_loss)
+    logs = tf.summary.merge_all()
+    for epoch in range(args.epochs):
+        perm = permutation(len(train_data))
+        no_iters = len(train_data)//args.batch_size
+        # no_iters=1
+        for it in range(no_iters):
+            batch_indices = perm[it*args.batch_size:(it+1)*args.batch_size]
+            batch_data = train_data[batch_indices]
+            batch_labels = train_labels[batch_indices]
+            if args.full_memory == 0:
+                memory_indices = np.random.choice(args.memory_size,
+                                                    args.batch_size,
+                                                    replace=False)
+            else:
+                memory_indices = list(range(args.memory_size))
+            _,objective_,logs_ = s.run([prof_optim_step,objective,logs],
+                            feed_dict={batch_data_:batch_data, 
+                                        batch_labels_:batch_labels,
+                                        memory_indices_:memory_indices})
+            logs_writer.add_summary(logs_, epoch*no_iters+it)
+            if args.verbose==1:
+                print(f'epoch[{epoch+1}] it[{it+1}/{no_iters}] '
+                        f'objective[{objective_}]')
+
+        if (epoch+1)%args.reset_freq==0:
+            s.run([reset_students,reset_train_optim])
+            if args.train_proj > 0:
+                train_steps = 0
+                while train_steps<300:
+                    train_steps+=1
+                    batch_indices = permutation(len(train_data))
+                    batch_indices = batch_indices[:args.batch_size]
+                    batch_data = train_data[batch_indices]
+                    batch_labels = train_labels[batch_indices]
+                    _, acc = s.run([train_optim_step,real_acc],
+                                feed_dict={batch_data_:batch_data,
+                                            batch_labels_:batch_labels})
+                    if acc>args.train_proj:
+                        break
+                print(f'finished training students steps[{train_steps}] '
+                        f'acc[{acc:.4f}]')
+
+    logs = tf.summary.merge([
+                        tf.summary.scalar('test_memory_fake_acc', fake_acc),
+                        tf.summary.scalar('test_memory_real_acc', real_acc),
+                        tf.summary.scalar('test_memory_real_loss',
+                                            total_real_loss),
+                        tf.summary.scalar('test_memory_fake_loss',
+                                            total_fake_loss)])
+
+    s.run([reset_students,reset_train_optim])
+    test_steps=max((args.test_epochs*args.memory_size//args.batch_size)+1,400)
+    # test_steps=1
+    for step in range(test_steps):
+        if args.batch_size>args.memory_size:
+            memory_indices = list(range(args.memory_size))
+        else:
+            memory_indices = np.random.choice(args.memory_size,args.batch_size,
+                                            replace=False)
+        # batch_indices = permutation(len(test_data))[:args.batch_size]
+        batch_data=test_data
+        batch_labels=test_labels
+        _, logs_ = s.run([test_optim_step, logs],
+                            feed_dict={batch_data_: batch_data,
+                                        batch_labels_: batch_labels,
+                                        memory_indices_:memory_indices,
+                                        training: False})
+        logs_writer.add_summary(logs_,step)
+
+    logs = tf.summary.merge([
+                        tf.summary.scalar('test_fake_acc', fake_acc),
+                        tf.summary.scalar('test_train_acc', real_acc),
+                        tf.summary.scalar('test_train_loss',
+                                            total_real_loss),
+                        tf.summary.scalar('test_fake_loss',
+                                            total_fake_loss),
+                        tf.summary.scalar('test_acc',test_acc),
+                        tf.summary.scalar('test_loss',total_test_loss)])
+
+    s.run([reset_students,reset_train_optim])
+    perm = permutation(args.memory_size)
+    for step in range(test_steps):
+        batch_indices = permutation(args.memory_size)[:args.batch_size]
+        batch_indices = perm[batch_indices]
+        batch_data = train_data[batch_indices]
+        batch_labels = train_labels[batch_indices]
+        memory_indices = permutation(args.memory_size)[:args.batch_size]
+        _, logs_ = s.run([train_optim_step,logs],
+                    feed_dict={batch_data_:batch_data,
+                                batch_labels_:batch_labels,
+                                memory_indices_: memory_indices,
+                                training: False,
+                                batch_test_data_: test_data,
+                                batch_test_labels_: test_labels})
+        logs_writer.add_summary(logs_,step)
+
+
+    fake_data_, fake_labels_,cov_ = s.run([fake_data, 
+                                    tf.nn.softmax(fake_labels),
+                                    covariance(fake_labels)])
+    for line in cov_:
+        print(line.tolist())
+    print(fake_labels_[:30])
+    tf_utils.simple_img(fake_data_,os.path.join(args.out_dir,'memory.png'),30)
+            
+
+def main():
+    args = read_config()
+    run(args)
+
+
+if __name__=='__main__':
+    main()
